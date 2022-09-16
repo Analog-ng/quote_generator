fed731bc0d18d43518c826c36b9ab8a6413fe73f
Oluwabamise Olatunji <tunjibamise94@gmail.com>
2022-09-16 03:11:33 +0100

    JS updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,20 @@
 // fetching quote from API
 
 async function getQuote() {
-    
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+        
+    } catch (error) {
+        console.log('whoops, no quote', error);
+    }
 }
 
 
-// ln oad
+// on load
 getQuote();
